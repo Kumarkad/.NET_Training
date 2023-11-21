c660f4d25cbde40f17d995073dea4098e7ac3ede
Kumar kad <64307080+Kumarkad@users.noreply.github.com>
2023-11-21 17:27:51 +0530

    Add files via upload

---
+++ Week1/sumoftwonumber/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter First Number : ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        
+        Console.Write("Enter Second Number : ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        double sum = num1 + num2;
+        double sumSqaure = sum * sum;
+        Console.Write("Square of sum is : "+sumSqaure);
+
+
+    }
+}
