b8f61c95fbeff02bff8a47144e6994d84af81d6b
Kumar kad <64307080+Kumarkad@users.noreply.github.com>
2023-11-21 17:25:24 +0530

    Delete StoreString directory

--- StoreString/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
-    {
-        string favcolor = "Red";
-        Console.WriteLine($"My favourite color is : {favcolor}");
-    }
-}
