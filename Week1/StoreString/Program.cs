1ed9aca08529c0af98fdf46e25ed35fd0b735d12
Kumar kad <64307080+Kumarkad@users.noreply.github.com>
2023-11-21 17:26:22 +0530

    Add files via upload

---
+++ Week1/StoreString/Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string favcolor = "Red";
+        Console.WriteLine($"My favourite color is : {favcolor}");
+    }
+}
